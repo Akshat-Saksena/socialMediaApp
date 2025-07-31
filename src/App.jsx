329005940396fc516424ca7b8196ed2dbf852b2c
Akshat Saksena <akshatsaksena8@gmail.com>
2025-07-31 23:26:27 +0530

    added site routing using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  let router = createBrowserRouter([
+    {
+      path: "/login",
+      Component: Login,
+    },
+    {
+      path: "/register",
+      Component: Register,
+    },
+  ]);
   return (
     <div>
-      <Register />
+      <RouterProvider router={router} />
     </div>
   );
 }
