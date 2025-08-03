7e4fc40404d5d52aca10bcc98e97e047c34caffa
Akshat Saksena <akshatsaksena8@gmail.com>
2025-08-03 17:50:48 +0530

    rightbar done

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -2,8 +2,98 @@ import "./rightbar.scss";
 
 function Rightbar() {
   return (
-    <div className="rBar">
-      <h1>Rightbar</h1>
+    <div className="rbar">
+      <div className="container">
+        <span>Suggestions</span>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+          <div className="respond">
+            <button>follow</button>
+            <button>dismiss</button>
+          </div>
+        </div>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+          <div className="respond">
+            <button>follow</button>
+            <button>dismiss</button>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <span>Latest Activites</span>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+          <p>changed their cover picture</p>
+          <span>1 min ago</span>
+        </div>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+          <p>liked a post</p>
+          <span>1 min ago</span>
+        </div>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+          <p>liked a comment</p>
+          <span>1 min ago</span>
+        </div>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+          <p>posted</p>
+          <span>1 min ago</span>
+        </div>
+      </div>
+      <div className="container">
+        <span>Online Friends</span>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+        </div>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+        </div>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+        </div>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+        </div>
+        <div className="item">
+          <div className="account">
+            <img src="https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg" />
+            <span>User Name</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
