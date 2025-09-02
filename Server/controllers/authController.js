051f67e7df31e49e8686e93c66e5c0941afce75d
Akshat Saksena <akshatsaksena8@gmail.com>
2025-09-02 19:40:24 +0530

    logout created on backend

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -20,6 +20,7 @@ export const login = async (req, res) => {
     res
       .cookie("accessToken", token, {
         httpOnly: true,
+        //secure:true
       })
       .status(200)
       .json(rest);
@@ -28,7 +29,15 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User has been logged out");
+};
 
 export const register = async (req, res) => {
   try {
